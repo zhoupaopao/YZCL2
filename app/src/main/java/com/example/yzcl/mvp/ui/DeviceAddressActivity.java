dd4b592db66d48d3e9cfb015b4ff9ec53102a37c
zhoupaopao <1312667058@qq.com>
2018-08-01 17:51:32 +0800

    3.0最新

--- app/src/main/java/com/example/yzcl/mvp/ui/DeviceAddressActivity.java
+++ app/src/main/java/com/example/yzcl/mvp/ui/DeviceAddressActivity.java
@@ -57,6 +57,7 @@ public class DeviceAddressActivity extends BaseActivity {
     }
 
     private void initView() {
+        title=findViewById(R.id.title);
         device_name=findViewById(R.id.device_name);
         waring_name=findViewById(R.id.waring_name);
         device_status=findViewById(R.id.device_status);
