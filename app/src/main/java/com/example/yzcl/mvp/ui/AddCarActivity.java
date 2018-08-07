e7eb43a2a320bc803433221fcccb25579340ab9d
zhoupaopao <1312667058@qq.com>
2018-08-07 17:37:41 +0800

    3.0最新

--- app/src/main/java/com/example/yzcl/mvp/ui/AddCarActivity.java
+++ app/src/main/java/com/example/yzcl/mvp/ui/AddCarActivity.java
@@ -230,6 +230,7 @@ public class AddCarActivity extends BaseActivity implements com.example.yzcl.uti
             case R.id.rl_cardtype:
                 //可以换成底部的
                 //CHU
+                ///kk
                 final List<TieBean> strings = new ArrayList<TieBean>();
                 strings.add(new TieBean("身份证"));
                 strings.add(new TieBean("组织机构代码"));
